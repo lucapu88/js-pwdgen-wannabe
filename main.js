c68b86a41d66d223383a437b6fe8ba9bfa2d095b
lucapu88 <caputoluca88@gmail.com>
2019-11-13 16:47:33 +0100

    abbellimento

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-alert('Ciao! ci servono alcuni dati per creare una password impeccabile')
-var nome = prompt('perfavore inserisci il tuo nome');
-var cognome = prompt('ora inserisci il tuo cognome');
+alert('Ciao! ci servono alcuni dati per creare una password impeccabileI clicca OK per continuare')
+var nome = prompt('Per favore inserisci il tuo nome');
+var cognome = prompt('Ora inserisci il tuo cognome');
 var colorePreferito = prompt("Qual'è il tuo colore preferito?");
 var password = ((nome + cognome + colorePreferito) + Math.random());
 document.getElementById('password').innerHTML = password;
