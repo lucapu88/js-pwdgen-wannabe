28829bd791b913f43dfbb3d2e06c2ece08dd69cb
lucapu88 <caputoluca88@gmail.com>
2019-11-13 17:05:11 +0100

    non funziona il display none/block

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ var cognome = prompt('Ora inserisci il tuo cognome');
 var colorePreferito = prompt("Qual'è il tuo colore preferito?");
 var password = ((nome + cognome + colorePreferito) + Math.random());
 document.getElementById('password').innerHTML = password;
+document.getElementById('contenitore').setAttribute('class','visibile');
