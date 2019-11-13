0be0aee64299d98ad851f579c55fa38317c89279
lucapu88 <caputoluca88@gmail.com>
2019-11-13 16:07:18 +0100

    inserimento di numeri random

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+alert('Ciao! ci servono alcuni dati per creare una password impeccabile')
 var nome = prompt('perfavore inserisci il tuo nome');
 var cognome = prompt('ora inserisci il tuo cognome');
-var colorePreferito = prompt("Qual'è il tuo colore preferito?")
-var password = ((nome + cognome + colorePreferito) + 88)
+var colorePreferito = prompt("Qual'è il tuo colore preferito?");
+var password = ((nome + cognome + colorePreferito) + Math.random());
 document.getElementById('password').innerHTML = password;
